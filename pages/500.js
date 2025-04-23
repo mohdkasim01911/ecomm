9773c1a845dd8bbacbc563e3e9cb8729c4e57038
Bstteam108 <bstteam108@gmail.com>
2025-04-23 11:38:36 +0430

    chnage

--- pages/500.js
+++ pages/500.js
@@ -3,6 +3,7 @@ const Error500 = dynamic(() => import("~/components/error/500"));
 
 export default function Custom500() {
   return <Error500 />;
+  // resolve issue 
 }
 
 Custom500.hasError = true;
