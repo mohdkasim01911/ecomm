f2aa8887ab125a8398851d39acfbb2fc9dbd5c16
mohdkasim01911 <81668626+mohdkasim01911@users.noreply.github.com>
2025-05-03 09:35:09 +0430

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const next_config = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["localhost","ecomm-lake.vercel.app"], // Add any external domains if needed
+    domains: ["localhost","ecomm-two-lemon.vercel.app"], // Add any external domains if needed
   },
   i18n: {
     defaultLocale: "en",
